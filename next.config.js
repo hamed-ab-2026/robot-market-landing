ed5eead80f8b1c08ba305af00eddc596794ce547
hamed@B <h.ab.web.dev@gmail.com>
2026-09-05 13:21:04 +0330

    feat:dockerized & test actions added

--- next.config.js
+++ next.config.js
@@ -1,16 +1,17 @@
-/** @type {import('next').NextConfig} */
-
 const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
 
 const nextConfig = {
     reactStrictMode: true,
-    // AntD ships CJS + uses some patterns that benefit from transpilation in the App Router.
-    transpilePackages: ['antd', '@ant-design/icons', 'rc-util', 'rc-pagination', 'rc-picker'],
 
+    transpilePackages: [
+        'antd',
+        '@ant-design/icons',
+        'rc-util',
+        'rc-pagination',
+        'rc-picker'
+    ],
 
-    output: 'export',
-
-    images: {unoptimized: true},
+    output: 'standalone',
 
     trailingSlash: true,
 
